fd217be7eca9d56cdd8947d31499fa6cce72e7bb
gabriellapriyanti <113757244+gabriellapriyanti@users.noreply.github.com>
2022-10-22 19:49:00 +0700

    PBO_P7

---
+++ Tax.java
@@ -0,0 +1,24 @@
+package PBO_P7;
+
+public class Tax {
+    double pajakGapok;
+    double pajakLembur;
+    double totalPajak;
+    double gajiBersih;
+
+    public double getPajakGapok() {
+        return pajakGapok;
+    }
+
+    public double getPajakLembur() {
+        return pajakLembur;
+    }
+
+    public double getTotalPajak() {
+        return totalPajak;
+    }
+
+    public double getGajiBersih() {
+        return gajiBersih;
+    }
+}
